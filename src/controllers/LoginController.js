7955e8c2010578fa54478b8f23da5bce14325929
AndresPrada <andres960.ap@gmail.com>
2023-06-05 20:50:39 -0500

    Empezando proyecto

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,62 @@
+function index(req, res) {
+  if (req.session.loggedin) {
+		// Output username
+    res.redirect('/');
+	
+  } else {
+    res.render('login/index');
+  }
+}
+
+function register(req, res) {
+  res.render('login/register');
+}
+function auth(req, res) {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  // Verificar si los campos email y password están presentes
+  if (!email || !password) {
+    return res.status(400).send('Email and password are required');
+  }
+
+  req.getConnection((err, conn) => {
+    if (err) {
+      // Error al conectarse a la base de datos u otro error interno
+      res.status(500).send('Internal Server Error');
+    } else {
+      const query = 'SELECT * FROM users WHERE email = ? AND password = ?';
+      conn.query(query, [email, password], (err, rows) => {
+        if (err) {
+          // Error al ejecutar la consulta
+          res.status(500).send('Internal Server Error');
+        } else {
+          if (rows.length > 0) {
+            console.log(rows);
+            res.status(200).send('Success');
+          } else {
+            console.log('not');
+            res.status(404).send('User Not Found');
+          }
+        }
+      });
+    }
+  });
+}
+
+
+
+function logout(req, res) {
+  if (req.session.loggedin) {
+    req.session.destroy();
+  }
+  res.redirect('/');
+}
+
+
+module.exports = {
+  index: index,
+  register: register,
+  auth: auth,
+  logout: logout,
+}
